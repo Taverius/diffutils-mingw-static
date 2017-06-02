4acae2dcabde5671caaa6ffbe2d005bfe8149e76
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2017-06-02 20:45:57 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,41 @@
+#! /bin/sh
+set -x
+
+pkgname=diffutils-3.3
+
+# get the file
+[[ ! -f "${pkgname}.tar.xz" ]] && wget http://ftp.gnu.org/gnu/diffutils/${pkgname}.tar.xz
+
+# extract the tarball
+[[ -d "${pkgname}" ]] && rm -rf "${pkgname}"
+tar xf ${pkgname}.tar.xz
+
+# apply patches
+cd ${pkgname}
+
+for file in ../patches/*.patch
+do
+    patch -p0 -i "$file"
+done
+
+cd ..
+
+# prep the output directory
+[[ -d "out/${MINGW_CHOST}" ]] && rm -rf "out/${MINGW_CHOST}"
+mkdir -p "out/${MINGW_CHOST}"
+
+# prep the build directory
+[[ -d "build-${pkgname}" ]] && rm -rf "build-${pkgname}"
+mkdir -p "build-${pkgname}"
+
+cd "build-${pkgname}"
+
+../${pkgname}/configure --prefix=$PWD/../out/${MINGW_CHOST} \
+    --host=${MINGW_CHOST} \
+    CFLAGS=-static LDFLAGS=-static
+
+make
+
+make install
+
+strip.exe ../out/${MINGW_CHOST}/bin/*.exe
